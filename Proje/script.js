3b6dda206dffc71632e9ae001c60ddb1bb402497
MuhammedFatihErcakir <muhammedfatihercakir@gmail.com>
2021-12-27 18:35:51 +0300

    Fix Project

--- Proje/script.js
+++ Proje/script.js
@@ -1,153 +1,55 @@
-class Calculator{
-    constructor(previousOperandTextElement, currentOperandTextElement) {
-        this.previousOperandTextElement = previousOperandTextElement
-        this.currentOperandTextElement = currentOperandTextElement
-        this.clear()
-    }
-
-    clear() {
-        this.currentOperand = ''
-        this.previousOperand = ''
-        this.operation = undefined
-    }
-
-    delete() {
-        this.currentOperand = this.currentOperand.toString().slice(0, -1)
-    }
-
-    appendNumber(number) {
-        if(number === '.' && this.currentOperand.includes('.'))
-        return this.currentOperand = this.currentOperand.toString() + number.toString()
-    }
-
-    chooseOperation(operation) {
-        if (this.currentOperand === '') return
-        if (this.previousOperand !== '') {
-            this.compute()
-        }
-        this.operation = operation
-        this.previousOperand = this.currentOperand
-        this.currentOperand = ''
-    }
-
-    compute() {
-        let computation
-        const prev = parseFloat(this.previousOperand)
-        const current = parseFloat(this.currentOperand)
-        if (isNaN(prev) || isNaN(current)) return
-        switch (this.operation) {
-            case '+':
-                computation = prev + current
-                break
-            case '-':
-                computation = prev - current
-                break
-            case '*':
-                computation = prev * current
-                break
-            case '/':
-                computation = prev / current
-                break
-            case 'sin':
-                computation = Math.sin(current)
-                break
-            case 'cos':
-                computation = Math.cos(current)
-                break
-            case 'tan':
-                computation = Math.tan(current)
-                break
-            case 'xⁿ':
-                computation = Math.pow(prev,current)
-                break
-            case '√':
-                computation = Math.sqrt(current)
-                break
-            case 'x²':
-                computation = current * current
-                break
-            case 'n!':
-                computation = 1
-                for (var i=1; i <= prev; i++){
-                    computation = computation * i;
-                }
-                break
-            default:
-                return
-        }
-        this.currentOperand = computation
-        this.operation = undefined
-        this.previousOperand = ''
-    }
-
-    getDisplayNumber(number) {
-        const stringNumber = number.toString()
-        const integerDigits = parseFloat(stringNumber.split('.')[0])
-        const decimalDigits = stringNumber.split('.')[1]
-        let integerDisplay
-        if (isNaN(integerDigits)) {
-            integerDisplay = ''
-        } else {
-            integerDisplay = integerDigits.toLocaleString('en', { maximumFractionDigits: 0 })
-        }
-        if (decimalDigits != null) {
-            return `${integerDisplay}.${decimalDigits}`
-        } else {
-        return integerDisplay
-        }    
-    }
-
-    updateDisplay() {
-        this.currentOperandTextElement.innerText = 
-            this.getDisplayNumber(this.currentOperand)
-        if (this.operation != null) {
-            this.previousOperandTextElement.innerText = 
-                `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
-        }
-        else {
-            this.previousOperandTextElement.innerText = ''
-        }
-    }
+function sin(){
+    document.calculator.result.value=Math.sin(document.calculator.result.value)
 }
 
-const numberButtons = document.querySelectorAll('[data-number]')
-const operationButtons = document.querySelectorAll('[data-operation]')
-const equalsButton = document.querySelectorAll('[data-equals]')
-const deleteButton = document.querySelectorAll('[data-delete]')
-const allClearButton = document.querySelectorAll('[data-all-clear]')
-const previousOperandTextElement = document.querySelector('[data-previous-operand]')
-const currentOperandTextElement = document.querySelector('[data-current-operand]')
+function cos(){
+    document.calculator.result.value=Math.cos(document.calculator.result.value)
+}
 
-const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+function tan(){
+    document.calculator.result.value=Math.tan(document.calculator.result.value)
+}
 
-numberButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        calculator.appendNumber(button.innerText)
-        calculator.updateDisplay()
-    })
-})
+function backSpace() {
+    var a = document.calculator.result.value;
+    document.calculator.result.value = a.substr(0,a.length-1);
+}
 
-operationButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        calculator.chooseOperation(button.innerText)
-        calculator.updateDisplay()
-    })
-})
+function square() {
+    document.calculator.result.value = Math.pow(document.calculator.result.value,2);
+}
 
-equalsButton.addEventListener('click', button => {
-    calculator.compute()
-    calculator.updateDisplay()
-})
+function sqrt() {
+    document.calculator.result.value = Math.sqrt(document.calculator.result.value)
+}
 
-allClearButton.addEventListener('click', button => {
-    calculator.clear()
-    calculator.updateDisplay()
-})
+function fakt() {
+    var faktorıyel = 1
+    for (var i = 1; i<= document.calculator.result.value; i++){
+        faktorıyel = faktorıyel * i;
+    }
+    
+    document.calculator.result.value = faktorıyel;
+}
 
-deleteButton.addEventListener('click', button => {
-    calculator.delete()
-    calculator.updateDisplay()
-})
+function number(value) {
+    document.calculator.result.value += value;
+}
+
+function remove() {
+    document.calculator.result.value = " ";
+}
+
+function equal() {
+    document.calculator.result.value = eval(document.calculator.result.value);
+}
+
+
+
+
+
+
+
 
 
 
